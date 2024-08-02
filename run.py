fe095564b39f56b7552e831d81023e54fd8c1f6f
Ana Ivanovic <ana.ivanovic0901@gmail.com>
2024-08-02 14:34:28 +0000

    Get a random number 1-6

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def roll_dice():
+    """
+    Simulates rolling a six-sided dice, get a random number from 1 to 6
+    """
+    return random.randint(1,6)
+    
